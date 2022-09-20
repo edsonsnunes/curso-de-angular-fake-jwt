0e83c02f797df151acd69eca830676b2f46a40b6
edsonsnunes <edsonnunes05@icloud.com>
2022-09-20 15:11:44 -0300

    20/09

--- src/app/core/components/auth/auth.module.ts
+++ src/app/core/components/auth/auth.module.ts
@@ -4,15 +4,9 @@ import { CommonModule } from "@angular/common";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { SignComponent } from "./pages/sign/sign.component";
 import { ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [SignComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
